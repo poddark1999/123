2b4558ec8cc1c91ef714239797630e06aa0cb023
hadri96 <68434862+hadri96@users.noreply.github.com>
2023-11-13 11:26:44 +0100

    Load and export instances methods moved to Controllers

--- controllers/model_controller.py
+++ controllers/model_controller.py
@@ -1,4 +1,20 @@
 from models.model import Model
+import pandas as pd
+import os
+from datetime import datetime
+
+def convert_type(value, type_):
+    '''
+    Typecasting function
+    '''
+    if type_ == str:
+        return str(value)
+    elif type_ == int:
+        return int(value)
+    elif type_ == float:
+        return float(value)
+    elif type == datetime:
+        return datetime.fromisoformat(value)
 
 class ModelController:
     """
@@ -8,6 +24,11 @@ class ModelController:
     Warning: Ensure that when inheriting from this class, override methods if necessary
     to provide specific functionality for the associated model.
     """
+
+    def __init__(self, cls=Model, name='Model', csv_path='models.csv'):
+        self.cls = cls
+        self.name = name
+        self.__csv_path = csv_path
 
     def create(self, **kwargs):
         """
@@ -69,4 +90,47 @@ class ModelController:
         Returns:
             List of model instances.
         """
-        pass
+        return self.cls.all
+
+    def export_instances(self):
+        '''
+        Class method to export instances to a csv file
+
+        '''
+        path_to_file = os.sep.join(['data', self.__csv_path])
+        df = pd.DataFrame([instance.__dict__ for instance in self.cls.all])
+        # renaming columns of private attributes
+        df.columns = [column.split('__')[-1] for column in df.columns]
+        df.to_csv(path_to_file, index=False)
+
+    def load_instances(self):
+        '''
+        Class method to load instances from a csv file
+
+        '''
+        # load the csv file
+        path_to_file = os.path.join('data', self.__csv_path)
+        ## check if file exists
+        if os.path.exists(path_to_file):
+            df = pd.read_csv(path_to_file)
+            attributes = {column: df[column] for column in df.columns}
+            if len(self.cls.data_types) != len(attributes):
+                raise AttributeError
+            f"Columns do not match the attributes of the class {self.name}"
+            for i in range(df.shape[0]):
+                attributes_instance = {column:values[i] for column, values in attributes.items()}
+                attributes_instance = {column: (value if isinstance(value, self.cls.data_types[column])
+                                            else convert_type(value, self.cls.data_types[column]))
+                                        for column, value in attributes_instance.items()}
+                Model(**attributes_instance)
+
+if __name__ == '__main__':
+
+    controller = ModelController()
+    controller.load_instances()
+
+    for _ in range(10):
+        Model()
+    for model in controller.list_all():
+        print(model.uuid)
+    controller.export_instances()
